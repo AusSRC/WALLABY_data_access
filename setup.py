3e0f0d59f929c2ee26f2d36f25e88357e869a3b3
Austin Shen <austin.shen@csiro.au>
2023-01-12 15:39:10 +0800

    self hosted + pypi on tag

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
+import os
+import re
 from setuptools import setup
+
+# Set version as environment variable
+semver_regex = re.compile(r'\d{1}.\d{1}.\d{1}')
+if 'VERSION' not in os.environ:
+    raise Exception('Set version as environment variable (e.g. export VERSION=1.0.0)')
+version = os.getenv('VERSION')
+match = semver_regex.search(version)
+if match is None:
+    raise Exception('Module version must follow semantic versioning format (e.g. v1.0.0)')
 
 setup(
     name='wallaby_data_access',
-    version='0.3.0',
+    version=match.group(),
     description='Module for accessing WALLABY internal release data ',
     url='https://github.com/AusSRC/WALLABY_data_access',
     author='Austin Shen',
