221563a1641ce9dfa125d5092d10af97aa7b957d
Austin Shen <austin.shen@csiro.au>
2023-01-16 16:54:40 +0800

    CI release + coverage
    *    Code coverage report
    *    Updated README
    *    PyPI release on tag

--- README.md
+++ README.md
@@ -4,14 +4,23 @@ Python module with tools for accessing internal release data from the WALLABY da
 
 ## Configuration
 
-Users will need to define a `.env` file to configure this module. The variables you will need to set (and default where possible) in this configuration are
+There are two requirements for accessing data via the module. They are:
+
+1. Clone of [WALLABY_database](https://github.com/AusSRC/WALLABY_database) repository
+2. Environment file with database credentials.
+
+The `.env` file requires:
 
 ```
-DJANGO_SECRET_KEY
-DJANGO_SETTINGS_MODULE
-DJANGO_ALLOW_ASYNC_UNSAFE = "True"
 DATABASE_HOST
 DATABASE_NAME
 DATABASE_USER
 DATABASE_PASS
+```
+
+Once these files are in your working directory you can specify them in the `connect()` function
+
+```
+import wallaby_data_access as wallaby
+wallaby.connect(db='<PATH_TO_WALLABY_database>', env='<PATH_TO_.env>')
 ```
--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+import os
+import re
+from pathlib import Path
 from setuptools import setup
+
+# Set version as environment variable
+semver_regex = re.compile(r'\d{1}.\d{1}.\d{1}')
+if 'RELEASE_VERSION' not in os.environ:
+    raise Exception('Set version as environment variable (e.g. export RELEASE_VERSION=1.0.0)')
+version = os.getenv('RELEASE_VERSION')
+match = semver_regex.search(version)
+if match is None:
+    raise Exception(f'Module version must follow semantic versioning format (e.g. v1.0.0). Provided: {version}')
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='wallaby_data_access',
-    version='0.3.0',
-    description='Module for accessing WALLABY internal release data ',
+    version=match.group(),
+    description='Module for accessing WALLABY internal release data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/AusSRC/WALLABY_data_access',
     author='Austin Shen',
     author_email='austin.shen@csiro.au',
