21640d6bbd04ea18e87323c83259ae7ffb7c89f1
Austin Shen <austin.shen@csiro.au>
2023-01-16 11:34:16 +0800

    RELEASE VERSION environment variable fix

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 # Set version as environment variable
 semver_regex = re.compile(r'\d{1}.\d{1}.\d{1}')
-if 'VERSION' not in os.environ:
-    raise Exception('Set version as environment variable (e.g. export VERSION=1.0.0)')
-version = os.getenv('VERSION')
+if 'RELEASE_VERSION' not in os.environ:
+    raise Exception('Set version as environment variable (e.g. export RELEASE_VERSION=1.0.0)')
+version = os.getenv('RELEASE_VERSION')
 match = semver_regex.search(version)
 if match is None:
-    raise Exception('Module version must follow semantic versioning format (e.g. v1.0.0)')
+    raise Exception(f'Module version must follow semantic versioning format (e.g. v1.0.0). Provided: {version}')
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
